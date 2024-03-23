fb3125a95cbd8159f644e5b72faf582885f4436b
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2024-03-23 16:49:22 -0300

    Exercicio 6 feito

--- Lista_Exercicio_LOOP.sql
+++ Lista_Exercicio_LOOP.sql
@@ -39,7 +39,7 @@ BEGIN
 END;*/
 
 -- EXERCICIO 5        
-BEGIN
+/*BEGIN
     FOR x IN (SELECT * FROM PRODUTO_COMPOSTO C INNER JOIN PRODUTO P ON (C.COD_PRODUTO = P.COD_PRODUTO) LEFT JOIN ESTOQUE_PRODUTO E ON (P.COD_PRODUTO = E.COD_PRODUTO)) LOOP
         IF x.COD_ESTOQUE IS NOT NULL THEN
             DBMS_OUTPUT.PUT_LINE('O nome do produto é: ' || x.NOM_PRODUTO || ' o seu código de barras: ' || x.COD_BARRA || ' código de estoque: ' || x.COD_ESTOQUE || ' data de estoque: ' || x.DAT_ESTOQUE);
@@ -47,9 +47,22 @@ BEGIN
             DBMS_OUTPUT.PUT_LINE('O nome do produto é: ' || x.NOM_PRODUTO || ' o seu código de barras: ' || x.COD_BARRA);
         END IF;
     END LOOP;
-END;
-      
-        
+END;*/
+-- EXERCICIO 6
+/*BEGIN
+    FOR x IN (SELECT * FROM PEDIDO P RIGHT JOIN CLIENTE C ON (P.COD_CLIENTE = C.COD_CLIENTE) WHERE COD_PEDIDO IS NOT NULL) LOOP
+        IF x.NOM_CLIENTE IS NOT NULL THEN
+            DBMS_OUTPUT.PUT_LINE('O código do pedido é: ' || x.COD_PEDIDO || ' a data do pedido é: ' || x.DAT_PEDIDO || ' o nome do cliente é: ' || x.NOM_CLIENTE || ' de cpf/CPJ: ' || x.NUM_CPF_CNPJ);
+        ELSE
+            DBMS_OUTPUT.PUT_LINE('O código do pedido é: ' || x.COD_PEDIDO || ' a data do pedido é: ' || x.DAT_PEDIDO);
+        END IF;
+    END LOOP;
+END;*/
+
+-- EXERCICIO 7
+DECLARE
+    VAR_ID_CLIENTE
+
         
         
         
